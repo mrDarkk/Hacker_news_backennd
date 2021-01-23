29051e4ad8c6df7aaf35bbbef1e1da2bf290ae6d
bhupendrabanothe <bhupendrabanothe@gmail.com>
2021-01-23 10:39:32 +0530

    feat: setup project :card_index: :tada:

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+require('dotenv/config');
+
+const app = express();
+
+app.use(cors())
+app.use(bodyParser.json())
+
+//Routes
+app.get('/', (req, res) => {
+    res.send('We are on HomePage')
+});
+
+// Connect MongoDB
+// mongoose.connect(process.env.DB_CONNECTION, { useUnifiedTopology: true, useNewUrlParser: true  }, () => {
+// 	console.log("Connected to DB")
+// });
+
+app.listen(3000);
